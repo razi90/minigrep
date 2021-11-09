ef1e2c7521b035a004942ee321dbc326f16b380c
Razi Escobar <andreas.razmyslov@gmail.com>
2021-11-09 22:00:03 +0100

    Moved program logic from main Improved error handling

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,11 @@
 use std::env;
 use std::fs;
 use std::process;
+use std::error::Error;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    
+
     let config = Config::new(&args).unwrap_or_else(|err| {
         println!("Problem parsing arguments: {}", err);
         process::exit(1);
@@ -13,10 +14,18 @@ fn main() {
     println!("Searching for {}", config.query);
     println!("In file {}", config.filename);
 
-    let contents = fs::read_to_string(config.filename)
-        .expect("Something went wrong reading the file");
+    if let Err(e) = run(config) {
+        println!("Application error: {}", e);
+        process::exit(1);
+    }
+}
+
+fn run(config: Config) -> Result<(), Box<dyn Error>> {
+    let contents = fs::read_to_string(config.filename)?;
 
     println!("With text:\n{}", contents);
+
+    Ok(())
 }
 
 struct Config {
@@ -31,7 +40,7 @@ impl Config {
         }
         let query = args[1].clone();
         let filename = args[2].clone();
-    
+
         Ok(Config { query, filename })
     }
 }
